1e1dcffb024de8c1d3f15f9f4e88d35a524a8d30
eldirb21 <eldirb95@gmail.com>
2025-06-21 13:00:35 +0700

    Feat: ios device setup

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ export default function App() {
       <KeyboardAvoidingView
         style={$flex}
         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 0}
       >
         <Home />
       </KeyboardAvoidingView>
